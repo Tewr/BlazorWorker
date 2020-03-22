0c19708f744210c3dac1cdb5a2c8e9c9e10c3d44
Tor KNUTSSON <tor.knutsson@qiminfo.ch>
2020-03-22 22:15:57 +0100

    Nugetify references

--- src/BlazorWorker.ServiceFactory.Shared/DefaultMessageSerializer.cs
+++ src/BlazorWorker.ServiceFactory.Shared/DefaultMessageSerializer.cs
@@ -1,20 +1,9 @@
-﻿using System.Text.Json;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace BlazorWorker.BackgroundServiceFactory
 {
     public class DefaultMessageSerializer : ISerializer
     {
-        /*public T Deserialize<T>(string objStr)
-        {
-            return JsonSerializer.Deserialize<T>(objStr);
-        }
-
-        public string Serialize(object obj)
-        {
-            return JsonSerializer.Serialize(obj);
-        }*/
-
         public T Deserialize<T>(string objStr)
         {
             return JsonConvert.DeserializeObject<T>(objStr);
@@ -26,33 +15,3 @@ namespace BlazorWorker.BackgroundServiceFactory
         }
     }
 }
-
-/*
-using Newtonsoft.Json;
-
-namespace BlazorWorker.BackgroundServiceFactory
-{
-    public class DefaultMessageSerializer : ISerializer
-    {
-        private static DefaultMessageSerializer _instance;
-
-        public static DefaultMessageSerializer Instance => _instance ?? (_instance = new DefaultMessageSerializer());
-
-        public static JsonSerializerSettings settings = new JsonSerializerSettings()
-        {
-            TypeNameHandling = TypeNameHandling.Objects
-        };
-        
-
-        public T Deserialize<T>(string objStr)
-        {
-            return JsonConvert.DeserializeObject<T>(objStr);
-        }
-
-        public string Serialize(object obj)
-        {
-            return JsonConvert.SerializeObject(obj);
-        }
-    }
-}
-*/
