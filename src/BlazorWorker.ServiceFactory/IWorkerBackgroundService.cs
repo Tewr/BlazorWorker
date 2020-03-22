67fb27fbcc9c790a424938153173634defaf0330
Tor KNUTSSON <tor.knutsson@qiminfo.ch>
2020-03-22 15:01:55 +0100

    Fixed example afte cleanup

--- src/BlazorWorker.ServiceFactory/IWorkerBackgroundService.cs
+++ src/BlazorWorker.ServiceFactory/IWorkerBackgroundService.cs
@@ -5,14 +5,12 @@ using System.Threading.Tasks;
 
 namespace BlazorWorker.BackgroundServiceFactory
 {
-    public interface IWorkerBackgroundService<T> where T : class
+    public interface IWorkerBackgroundService<T>: IAsyncDisposable where T : class
     {
         Task<Shared.EventHandle> RegisterEventListenerAsync<TResult>(string eventName, EventHandler<TResult> myHandler);
         Task<TResult> RunAsync<TResult>(Expression<Func<T, TResult>> function);
         Task RunAsync(Expression<Action<T>> action);
 
         IWorkerMessageService GetWorkerMessageService();
-
-        ValueTask DisposeAsync();
     }
 }
