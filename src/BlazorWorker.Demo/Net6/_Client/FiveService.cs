d8c96af7d876a2417ceba395a4aa75e622b9d543
Tor KNUTSSON <Tewr@users.noreply.github.com>
2022-01-08 00:14:58 +0100

    Full test suite for .NET 6 Fixed Failing IoC Examples

---
+++ src/BlazorWorker.Demo/Net6/_Client/FiveService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWorker.Demo.Client
+{
+    public class FiveService
+    {
+        public int Five() => 5;
+    }
+}
---
+++ src/BlazorWorker.Demo/SharedPages/Shared/NavMenuLinksModel.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorWorker.Demo.SharedPages.Shared
+{
+    public class NavMenuLinksModel
+    {
+        static string BlazorWorkerVersion { get; } =
+        $"v{typeof(BlazorWorker.BackgroundServiceFactory.WorkerBackgroundServiceExtensions).Assembly.GetName().Version}";
+
+        static string BlazorCoreWorkerVersion { get; } =
+          $"v{typeof(BlazorWorker.WorkerCore.IWorkerMessageService).Assembly.GetName().Version}";
+
+        static string BlazorWorkerJSRuntimeVersion { get; } =
+          $"v{typeof(BlazorWorker.Extensions.JSRuntime.BlazorWorkerJSRuntime).Assembly.GetName().Version}";
+
+
+        public static IEnumerable<NavMenuLinkInfo> NavMenuLinks { get; } = new List<NavMenuLinkInfo>()
+        {
+            {
+                new() { Icon = "cog", Href="", Text = "Simple Worker", Match= NavLinkMatch.All } 
+            },
+            {
+                new() { Icon = "copywriting", Href="BackgroundServiceMulti", Text = "Multiple Workers" }
+            },
+            {
+                new() { Icon = "command", Href="CoreExample", Text = "Core Example" }
+            },
+            {
+                new() { Icon = "globe", Href="Http", Text = "HttpClient Example" }
+            },
+            {
+                new() { Icon = "transfer", Href="IoCExample", Text = "IoC / DI Example" }
+            },
+            {
+                new() { Icon = "document", Href="IndexedDB", Text = "IndexedDB" }
+            },
+            {
+                new() { Icon = "fork", Href="https://github.com/tewr/BlazorWorker", Text = "To the source!" }
+            },
+            {
+                new() { Icon = "info", Href="https://www.nuget.org/packages/Tewr.BlazorWorker.BackgroundService", Small=true, Text = $"BackgroundService {BlazorWorkerVersion}" }
+            },
+            {
+                new() { Icon = "info", Href="https://www.nuget.org/packages/Tewr.BlazorWorker.Core", Small=true, Text = $"Core {BlazorCoreWorkerVersion}" }
+            },
+            {
+                new() { Icon = "info", Href="https://www.nuget.org/packages/Tewr.BlazorWorker.Extensions.JSRuntime", Small=true, Text = $"JSRuntime {BlazorWorkerJSRuntimeVersion}" }
+            }
+        };
+    }
+
+    public class NavMenuLinkInfo
+    {
+        
+        public string Icon { get; set; }
+
+        
+        public string Href { get; set; }
+
+        public bool Small { get; set; }
+
+        
+        public NavLinkMatch Match { get; set; } = NavLinkMatch.Prefix;
+        public string Text { get; internal set; }
+    }
+}
