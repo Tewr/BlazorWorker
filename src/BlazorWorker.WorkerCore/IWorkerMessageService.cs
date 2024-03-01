9ed55c82b8f3169f1287b52ca871b565a9172937
Tewr <Tewr@users.noreply.github.com>
2024-03-01 13:53:25 +0100

    Some docs for worker message service

--- src/BlazorWorker.WorkerCore/IWorkerMessageService.cs
+++ src/BlazorWorker.WorkerCore/IWorkerMessageService.cs
@@ -5,10 +5,23 @@ namespace BlazorWorker.WorkerCore
 {
     public interface IWorkerMessageService
     {
+        /// <summary>
+        /// Events for incoming messages to the current context
+        /// </summary>
         event EventHandler<string> IncomingMessage;
 
+        /// <summary>
+        /// Post a message to the context this message service belongs to
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         Task PostMessageAsync(string message);
 
+        /// <summary>
+        /// Post a message that can be read directly on the main js thread using the "blazorworker:jsdirect" event on the window object
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         Task PostMessageJsDirectAsync(string message);
     }
 }
