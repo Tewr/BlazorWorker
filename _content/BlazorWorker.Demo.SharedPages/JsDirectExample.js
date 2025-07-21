b1ded637b22e4a31c81afa0965216cb3434ca3e4
Tewr <Tewr@users.noreply.github.com>
2025-07-21 22:43:03 +0200

    .net9 demo

---
+++ _content/BlazorWorker.Demo.SharedPages/JsDirectExample.js
@@ -0,0 +1,14 @@
+﻿// This 'classic' script will run on the main thread and cirectly receieve message from the worker
+function setupJsDirectForWorker(myWorkerId) {
+    const currtime = () => [new Date()].map(d => `${d.toTimeString().split(" ")[0]}:${d.getMilliseconds()}`)[0];
+    const output = document.getElementById('jsDirectOutputElement');
+    output.innerText += `\n${currtime()} Setting up event listener.`;
+    window.addEventListener('blazorworker:jsdirect', function (e) {
+        if (e.detail.workerId === myWorkerId) {
+            output.innerText += `\n${currtime()} blazorworker:jsdirect listener. workerId: ${e.detail.workerId}. data: '${e.detail.data}'`;
+        }
+        else {
+            console.log('blazorworker:jsdirect handler for some other worker not handled by this listener', { workerId: e.detail.workerId, data: e.detail.data });
+        }
+    });
+}
---
+++ _content/BlazorWorker.Demo.SharedPages/JsInteractionsExample.js
@@ -0,0 +1,11 @@
+﻿// This script will run on the worker.
+self.jsInteractionsExample = async (JsInteractionsExampleWorkerService) => {
+    console.log(`Interacting with worker Js on worker thread.`);
+    await JsInteractionsExampleWorkerService.invokeMethodAsync("CallbackFromJavascript", 'Callback to dotnet instance method');
+
+    // Calling static methods can be done like this
+    var demoExports = await self.BlazorWorker.getAssemblyExports("BlazorWorker.Demo.Shared")
+    var methodResult = demoExports.BlazorWorker.Demo.Shared.JsInteractionsExampleWorkerService.StaticCallbackFromJs("Static methods are cheaper to call.");
+
+    await JsInteractionsExampleWorkerService.invokeMethodAsync("CallbackFromJavascript", 'StaticCallbackFromJs returned: '+ methodResult);
+}
